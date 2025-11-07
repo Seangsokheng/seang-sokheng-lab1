b9ec82af8c508055c93502a2c266f818d3b4db35
Seang Sokheng <wwhengww@gmail.com>
2025-11-07 15:22:36 +0700

    workflow deploy to railway

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const app = express(); 
 app.get('/', (req, res) => { 
-res.send('Hello, CI/CD! deployed via GitHub Actions and Railway.'); 
+res.send('Hello, CI/CD! deployed via GitHub Actions and Railway work work.'); 
 }); 
 module.exports = app;
